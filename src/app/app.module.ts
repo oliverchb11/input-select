d9e6e3df2c5bd6beaf3a6405360fd1e83ad89e25
Oliver Fernando Charry Benavides <olivercharry@seti.com.co>
2023-05-08 20:23:50 -0500

    librerias para implementar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentesModule } from 'projects/componentes/src/public-api';
+import { InputSelectModule } from 'projects/input-select/src/public-api';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputSelectModule,
+    ComponentesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
